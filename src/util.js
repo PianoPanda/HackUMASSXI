4fc980abfbeae16823a3de297ef0cd676c95928c
Maxwell Tang <amoinus@gmail.com>
2023-11-11 12:02:39 -0500

    Added support for bigints in format32

--- src/util.js
+++ src/util.js
@@ -1,15 +1,25 @@
 /**
  * Formats a number so that it fits within `[0x0, 0xFFFF_FFFF]`.
- * @param {number} value Must be an integer.
+ * @param {number | bigint} value Must be an integer.
  * @returns {number}
  */
 export function format32(value) {
-    if(!Number.isInteger(value)) throw new Error(`format32 called on non-integer value [${value}].`);
-    if(!Number.isSafeInteger(value)) throw new Error(`format32 called on unsafe value [${value}].`);
-    while(value < 0){
-        value += 0x1_0000_0000;
+    switch (typeof value) {
+        case "number":
+            if (!Number.isInteger(value)) throw new Error(`format32 called on non-integer value [${value}].`);
+            if (!Number.isSafeInteger(value)) throw new Error(`format32 called on unsafe value [${value}].`);
+            while (value < 0) {
+                value += 0x1_0000_0000;
+            }
+            return Number(value % 0x1_0000_0000)
+        case "bigint":
+            while (value < 0n) {
+                value += 0x1_0000_0000n;
+            }
+            return Number(value % 0x1_0000_0000n)
+        default:
+            throw new Error(`format32 called on non-number value ${value.toString()}`)
     }
-    return value % 0x1_0000_0000
 }
 
 /**
@@ -17,7 +27,7 @@ export function format32(value) {
  * @param {number} value Must be an integer.
  * @returns {string} 
  */
-export function toBinary(value){
+export function toBinary(value) {
     return format32(value).toString(2).padStart(32, 0);
 }
 
@@ -27,7 +37,7 @@ export function toBinary(value){
  * @param {number} b
  */
 export function compuns(a, b) {
-  let [_a, __a, _b, __b] = [a >>> 1, a & 1, b >>> 1, b & 1]
-  if (_a !== _b) return _a - _b
-  else return __a == __b ? 0 : __a - __b
+    let [_a, __a, _b, __b] = [a >>> 1, a & 1, b >>> 1, b & 1]
+    if (_a !== _b) return _a - _b
+    else return __a == __b ? 0 : __a - __b
 }
