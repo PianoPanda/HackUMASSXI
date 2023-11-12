e7bb8b68ef78438050769e8e4fe99ec7471e4c01
Maxwell Tang <amoinus@gmail.com>
2023-11-12 09:57:16 -0500

    Some elves are joining santas workshop

---
+++ testfileartifacts/santasworkshop/hello.c
@@ -0,0 +1,27 @@
+int putchar(int c){
+    asm volatile(
+        "add a0,%0,x0;"
+        ".word 0x80051073;"
+        ::"r"(c)
+        :"a0"
+    );
+    // 0b100000000000_11111_001_00000_1110011
+    return 0;
+}
+
+void shutdown(){
+    asm (".word 0x80105073"); //shutdown
+}
+
+void print_string(char *str){
+    while (*str)
+    {
+        putchar(*str);
+        str++;
+    }
+}
+
+int main() {
+    print_string("Hello World!\n");
+    shutdown();
+}
